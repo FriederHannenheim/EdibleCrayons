f467ca9bb8ec7c71e452ffe559fc3b094ab627fe
Frieder Hannenheim <frieder12.fml@gmail.com>
2020-08-08 18:27:56 +0200

    Moved the crayons in a separate creative tab

--- src/main/java/com/fhannenheim/ediblecrayons/EdibleCrayons.java
+++ src/main/java/com/fhannenheim/ediblecrayons/EdibleCrayons.java
@@ -3,6 +3,7 @@ package com.fhannenheim.ediblecrayons;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.item.*;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -14,16 +15,19 @@ import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.ForgeRegistries;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import sun.awt.FontDescriptor;
 
+import java.awt.*;
 import java.util.stream.Collectors;
 
 @Mod("ediblecrayons")
 public class EdibleCrayons
 {
     public EdibleCrayons() {
+
         MinecraftForge.EVENT_BUS.register(this);
     }
 
@@ -33,11 +37,23 @@ public class EdibleCrayons
         public static void registerItems(final RegistryEvent.Register<Item> event){
             for(DyeColor color : DyeColor.values()){
                 event.getRegistry().register(new Item(new Item.Properties()
-                        .group(ItemGroup.FOOD)
+                        .group(CrayonItemGroup.instance)
                         .food(new Food.Builder().saturation(10).hunger(10).build())
                 ).setRegistryName(color.getTranslationKey()+"_crayon"));
             }
 
+        }
+    }
+    public static class CrayonItemGroup extends ItemGroup{
+        public static final CrayonItemGroup instance = new CrayonItemGroup(ItemGroup.GROUPS.length,"crayons");
+        private CrayonItemGroup(int index, String label){
+            super(index, label);
+        }
+
+        @Override
+        public ItemStack createIcon() {
+            return new ItemStack(ForgeRegistries.ITEMS.getValue(
+                    new ResourceLocation("ediblecrayons","red_crayon")));
         }
     }
 }
