943f828994bfb419fe2fff7131063828af6c40be
Frieder Hannenheim <frieder12.fml@gmail.com>
2020-09-08 16:25:14 +0200

    Fixed last commit

--- src/main/java/com/fhannenheim/ediblecrayons/EdibleCrayons.java
+++ src/main/java/com/fhannenheim/ediblecrayons/EdibleCrayons.java
@@ -1,27 +1,13 @@
 package com.fhannenheim.ediblecrayons;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
 import net.minecraft.item.*;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.ForgeRegistries;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import sun.awt.FontDescriptor;
-
-import java.awt.*;
-import java.util.stream.Collectors;
+import net.minecraftforge.registries.IForgeRegistry;
 
 @Mod("ediblecrayons")
 public class EdibleCrayons
@@ -38,7 +24,7 @@ public class EdibleCrayons
                     .food(new Food.Builder().saturation(10).hunger(10).build());
 
         for(DyeColor color : DyeColor.values()){
-            registry.register(new Item(properties))
+            registry.register(new Item(properties)
                 .setRegistryName(color.getTranslationKey()+"_crayon"));
         }
     }
